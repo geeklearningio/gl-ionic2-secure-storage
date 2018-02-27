4b05ddd163e4492085cc13aa020e8e0122100024
Michel Vidailhet <michel.vidailhet@gmail.com>
2018-02-27 18:34:48 +0100

    trying to have private providers

--- src/gl-ionic2-secure-storage.module.ts
+++ src/gl-ionic2-secure-storage.module.ts
@@ -3,18 +3,18 @@ import {GLSecureStorageProvider} from "./providers/gl-secure-storage-provider";
 import {GLSecureStorageConfigProvider} from "./providers/gl-secure-storage-config-provider";
 import {CryphoSecureStorageProvider} from "./providers/platform-storages/crypho-secure-storage-provider";
 import {IntelSecureStorageProvider} from "./providers/platform-storages/intel-secure-storage-provider";
+import {StorageProvider} from "./providers/platform-storages/storage-provider";
 
 @NgModule({
     declarations: [],
-    exports: [
-        
-    ]
+    exports: [],
+    providers: [CryphoSecureStorageProvider, IntelSecureStorageProvider, StorageProvider ]
 })
 export class GlIonic2SecureStorageModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: GlIonic2SecureStorageModule,
-      providers: [ GLSecureStorageProvider, GLSecureStorageConfigProvider, CryphoSecureStorageProvider, IntelSecureStorageProvider ]
+      providers: [ GLSecureStorageProvider, GLSecureStorageConfigProvider ]
     };
   }
 }
