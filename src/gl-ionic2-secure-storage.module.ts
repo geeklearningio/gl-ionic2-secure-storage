39d68af694540e032701e189cc4a06da628efc08
Michel Vidailhet <michel.vidailhet@gmail.com>
2018-02-27 15:54:33 +0100

    forgot to include platform specific providers

--- src/gl-ionic2-secure-storage.module.ts
+++ src/gl-ionic2-secure-storage.module.ts
@@ -1,6 +1,8 @@
 import {NgModule, ModuleWithProviders} from "@angular/core";
 import {GLSecureStorageProvider} from "./providers/gl-secure-storage-provider";
 import {GLSecureStorageConfigProvider} from "./providers/gl-secure-storage-config-provider";
+import {CryphoSecureStorageProvider} from "./providers/platform-storages/crypho-secure-storage-provider";
+import {IntelSecureStorageProvider} from "./providers/platform-storages/intel-secure-storage-provider";
 
 @NgModule({
     declarations: [
@@ -12,7 +14,7 @@ export class GlIonic2SecureStorageModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: GlIonic2SecureStorageModule,
-      providers: [ GLSecureStorageProvider, GLSecureStorageConfigProvider ]
+      providers: [ GLSecureStorageProvider, GLSecureStorageConfigProvider, CryphoSecureStorageProvider, IntelSecureStorageProvider ]
     };
   }
 }
